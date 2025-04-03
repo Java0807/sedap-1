522634ea754e13c9075cea3decce01946c4054c8
Java0807 <uzjava0ff@gmail.com>
2025-04-03 14:58:16 +0500

    hello

--- src/components/pages/customers/CustomersList.js
+++ src/components/pages/customers/CustomersList.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import PageTitle from '@/components/common/PageTitle'
+import React from "react";
+import PageTitle from "@/components/common/PageTitle";
 
 function CustomersList() {
   return (
     <div>
-        <PageTitle title="General Customer" subtitle="Here is your general customers list data" />
+      <PageTitle
+        title="General Customer"
+        subtitle="Here is your general customers list data"
+      />
+      <h1>hello</h1>
     </div>
-  )
+  );
 }
 
-export default CustomersList
+export default CustomersList;
